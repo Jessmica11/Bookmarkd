7e132aa770200e38ad729a2a808dc0cdc90ca779
Jessmica11 <116199836+Jessmica11@users.noreply.github.com>
2024-02-29 17:46:38 -0500

    adding a server folder with starting files we may need

---
+++ server/utils/auth.js
@@ -0,0 +1,10 @@
+const { GraphQLError } = require('graphql');
+const jwt = require('jsonwebtoken');
+
+// double-check this (JS)
+const secret = 'B00kmarkd!';
+const expiration = '2h';
+
+module.exports = { 
+
+};
