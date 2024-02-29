c94ea8dd5d5b0c603bda4a70b11210914793fa61
Jessmica11 <116199836+Jessmica11@users.noreply.github.com>
2024-02-29 18:16:29 -0500

    error mark, attempting to fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
-import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom/dist'
-import './index.css'
+import ReactDOM from 'react-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.css';
 
-import App from './App.jsx'
-import Home from './pages/Home';
-import Signup from './pages/Signup';
-import Login from './pages/Login';
-import Profile from './pages/Profile';
-import Error from './pages/Error';
+import App from './src/App.jsx';
+import Home from './src/pages/Home';
+import Signup from './src/pages/Signup';
+import Login from './src/pages/Login';
+import Profile from './src/pages/Profile';
+import Error from './src/pages/Error';
+import Bookclub from './src/pages/Bookclub'; // Import Bookclub if it's not imported
 
 const router = createBrowserRouter([
   {
@@ -17,31 +18,38 @@ const router = createBrowserRouter([
     children: [
       {
         index: true,
-        element: <Home />
-      }, {
+        element: <Home />,
+      },
+      {
         path: '/login',
-        element: <Login />
-      }, {
+        element: <Login />,
+      },
+      {
         path: '/signup',
-        element: <Signup />
-      }, {
+        element: <Signup />,
+      },
+      {
         path: '/me',
-        element: <Profile />
-      }, {
+        element: <Profile />,
+      },
+      {
         path: '/profiles/:profileId',
-        element: <Profile />
-      }, {
+        element: <Profile />,
+      },
+      {
         path: '/bookclubs/:bookclubId',
-        element: <Bookclub />
-      }
+        element: <Bookclub />,
+      },
       {
         path: '/bookclubs/:bookclubId/comments',
-        element: <Bookclub />
-      }
-    ]
-  }
-])
+        element: <Bookclub />,
+      },
+    ],
+  },
+]);
+
+import { RouterProvider } from 'react-router-dom'; // Import RouterProvider
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <RouterProvider router={router} />
-)
+);
