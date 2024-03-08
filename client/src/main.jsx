3bbf2c6892d14374f9f60ba5ba7f00d254b069d0
Jessmica11 <116199836+Jessmica11@users.noreply.github.com>
2024-03-07 21:57:01 -0500

    added the auth page route to the main.jsx so you can view

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,17 +2,16 @@ import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import App from './App.jsx';
-import Home from './pages/Home';
+import Auth from './pages/Auth.jsx';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
-    errorElement: <NotFound />,
     children: [
       {
-        index: true,
-        element: <Home />
+        path: '/auth',
+        element: <Auth />
       },
     ],
   },
