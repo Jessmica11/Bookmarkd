11baad03c0f725b975f091bd32e3b858c0b3254c
Jessmica11 <116199836+Jessmica11@users.noreply.github.com>
2024-03-07 21:57:49 -0500

    added auth.js that handles jwt token in local storage

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -1,1 +1,59 @@
-import decode from 'jwt-decode';
+import * as decode from 'jwt-decode';
+
+class AuthService {
+  // get user profile information from the decoded token
+  getProfile() {
+    return decode(this.getToken());
+  }
+
+  // check if the user is logged in
+  loggedIn() {
+    const token = this.getToken();
+    return !!token && !this.isTokenExpired(token);
+  }
+
+  // see if the token is expired
+  isTokenExpired(token) {
+    try {
+      const decoded = decode(token);
+      if (decoded.exp < Date.now() / 1000) {
+        return true;
+      } else return false;
+    } catch (err) {
+      return false;
+    }
+  }
+
+  // get the user token from local storage
+  getToken() {
+    return localStorage.getItem('id_token');
+  }
+
+  // Login user and redirect to their profile page
+  login(idToken, userId, redirectPath = '/') {
+    localStorage.setItem('id_token', idToken);
+
+    // call the server route to handle redirection
+    fetch(`/api/redirect/${userId}`)
+      .then((response) => response.json())
+      .then((data) => {
+        const { message, redirectPath } = data;
+        if (message) {
+          console.error(message);
+        } else {
+          window.location.assign(redirectPath);
+        }
+      })
+      .catch((error) => {
+        console.error('Error fetching redirection information:', error);
+      });
+  }
+
+  // log the user out and redirect to the authentication page (will show login form once state updates)
+  logout(redirectPath = '/authentication') {
+    localStorage.removeItem('id_token');
+    window.location.assign(redirectPath);
+  }
+}
+
+export default new AuthService();
