848959f67d89fcd48d0b4a92f825dd7ea2822a5f
Jessmica11 <116199836+Jessmica11@users.noreply.github.com>
2024-03-09 20:49:21 -0500

    latest version

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,5 +1,6 @@
 const { GraphQLError } = require('graphql');
 const jwt = require('jsonwebtoken');
+const { User } = require('../models');
 
 const secret = 'B00kmarkd!';
 const expiration = '2h';
@@ -21,7 +22,55 @@ function authMiddleware(req, res, next) {
   }
 
   try {
-    // Verify the token
+    // verify that the token is valid and not expired
+    const decoded = verifyToken(token.replace('Bearer ', ''));
+
+    // attach the decoded user information to the request object
+    req.user = decoded;
+
+    next();
+  } catch (err) {
+    return next(new GraphQLError('Invalid token or token has expired.'));
+  }
+}
+
+async function authenticateUser(email, password) {
+  // find the user by the email
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    return null;
+  }
+
+  // check if the provided password is correct
+  const isCorrectPassword = await user.isCorrectPassword(password);
+
+  if (!isCorrectPassword) {
+    return null;
+  }
+
+  // return the user if the email and password are correct
+  return user;
+}
+
+function signToken({ id, email, username }) {
+  return jwt.sign({ id, email, username }, secret, { expiresIn: expiration });
+}
+
+function verifyToken(token) {
+  return jwt.verify(token, secret);
+}
+
+function authMiddleware(req, res, next) {
+  // pull the token from the request headers or our query string
+  const token = req.headers.authorization || req.query.token;
+
+  if (!token) {
+    return next(new GraphQLError('Authentication token is missing.'));
+  }
+
+  try {
+    // verify the token we made earlier
     const decoded = verifyToken(token.replace('Bearer ', ''));
 
     // attach the decoded user information to the request object
@@ -37,4 +86,5 @@ module.exports = {
   signToken,
   verifyToken,
   authMiddleware,
+  authenticateUser,
 };
