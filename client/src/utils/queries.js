e620c4d08b22cd40948103de4199d66fdfd44963
Jessmica11 <116199836+Jessmica11@users.noreply.github.com>
2024-03-10 18:23:47 -0400

    Add files via upload

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,35 +1,7 @@
 import { gql } from '@apollo/client';
 
-export const GET_BOOK_CLUB_DETAILS = gql`
-  query getBookClubDetails($bookClubId: ID!) {
-    bookClubById(id: $bookClubId) {
-      _id
-      name
-      description
-      members {
-        _id
-        username
-      }
-      books {
-        _id
-        title
-        author
-      }
-      comments {
-        _id
-        content
-        user {
-          _id
-          username
-        }
-        timestamp
-      }
-    }
-  }
-`;
-
-export const GET_ALL_BOOK_CLUBS = gql`
-  query getAllBookClubs {
+export const QUERY_BOOK_CLUBS = gql`
+  query GetBookClubs {
     bookClubs {
       _id
       name
@@ -37,11 +9,20 @@ export const GET_ALL_BOOK_CLUBS = gql`
       members {
         _id
         username
+        email
+        bio
+        book_clubs {
+          _id
+          name
+        }
+        comments {
+          _id
+          content
+          timestamp
+        }
       }
       books {
         _id
-        title
-        author
       }
       comments {
         _id
@@ -49,6 +30,12 @@ export const GET_ALL_BOOK_CLUBS = gql`
         user {
           _id
           username
+          email
+          bio
+          book_clubs {
+            _id
+            name
+          }
         }
         timestamp
       }
@@ -56,9 +43,52 @@ export const GET_ALL_BOOK_CLUBS = gql`
   }
 `;
 
-export const GET_USER_DETAILS = gql`
-  query getUserDetails($userId: ID!) {
-    userById(id: $userId) {
+export const QUERY_BOOK_CLUB_BY_ID = gql`
+  query GetBookClubById($id: ID!) {
+    bookClubById(id: $id) {
+      _id
+      name
+      description
+      members {
+        _id
+        username
+        email
+        bio
+        book_clubs {
+          _id
+          name
+        }
+        comments {
+          _id
+          content
+          timestamp
+        }
+      }
+      books {
+        _id
+      }
+      comments {
+        _id
+        content
+        user {
+          _id
+          username
+          email
+          bio
+          book_clubs {
+            _id
+            name
+          }
+        }
+        timestamp
+      }
+    }
+  }
+`;
+
+export const QUERY_USERS_IN_BOOK_CLUB = gql`
+  query GetUsersInBookClub($bookClubId: ID!) {
+    usersInBookClub(bookClubId: $bookClubId) {
       _id
       username
       email
@@ -70,50 +100,59 @@ export const GET_USER_DETAILS = gql`
       comments {
         _id
         content
-        bookClub {
-          _id
-          name
-        }
         timestamp
       }
     }
   }
 `;
 
-export const GET_ALL_USERS = gql`
-  query getAllUsers {
-    users {
-      _id
-      username
-      email
-      bio
-      book_clubs {
-        _id
-        name
-      }
-      comments {
-        _id
-        content
-        bookClub {
-          _id
-          name
-        }
-        timestamp
-      }
-    }
-  }
-`;
-
-export const GET_BOOK_CLUB_COMMENTS = gql`
-  query getBookClubComments($bookClubId: ID!) {
+export const QUERY_BOOK_CLUB_COMMENTS = gql`
+  query GetBookClubComments($bookClubId: ID!) {
     bookClubComments(bookClubId: $bookClubId) {
       _id
       content
       user {
         _id
         username
+        email
+        bio
+        book_clubs {
+          _id
+          name
+        }
       }
       timestamp
+    }
+  }
+`;
+
+export const QUERY_CURRENT_USER = gql`
+  query GetCurrentUser {
+    me {
+      _id
+      username
+      email
+      bio
+      book_clubs {
+        _id
+        name
+      }
+      comments {
+        _id
+        content
+        timestamp
+      }
+    }
+  }
+`;
+
+export const QUERY_USER_NOTIFICATIONS = gql`
+  query GetUserNotifications($userId: ID!) {
+    notifications(userId: $userId) {
+      _id
+      message
+      createdAt
+      // Additional notification-related fields
     }
   }
 `;
