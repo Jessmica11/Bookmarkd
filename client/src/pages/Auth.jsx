255466efb8d8ee3f09696479d0c6a8fbaa9e9744
Jessmica11 <116199836+Jessmica11@users.noreply.github.com>
2024-03-10 18:55:07 -0400

    user will get redirected if not logged in

--- client/src/pages/Auth.jsx
+++ client/src/pages/Auth.jsx
@@ -1,10 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Tabs, Tab, Container, Row, Col } from 'react-bootstrap';
+import { useHistory, useLocation } from 'react-router-dom';
 import Login from '../components/Login/Login'; 
 import SignUp from '../components/SignUp/SignUp';
+import Auth from '../utils/auth';
 
 const Auth = () => {
   const [key, setKey] = useState('login');
+  const history = useHistory();
+  const location = useLocation();
+
+  useEffect(() => {
+    // Redirect to login tab if user is not logged in and trying to access other pages
+    if (!Auth.loggedIn() && location.pathname !== '/auth') {
+      history.push('/auth');
+      setKey('login');
+    }
+  }, [location, history]);
 
   const handleTabSelect = (selectedKey) => {
     setKey(selectedKey);
