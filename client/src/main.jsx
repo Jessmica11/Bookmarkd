c7e5b1e7fa8ec5d929364b32dd24ed47d3955ff1
Jessmica11 <116199836+Jessmica11@users.noreply.github.com>
2024-03-10 18:39:05 -0400

    main.jsx edits

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,28 +1,22 @@
-import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import App from './App.jsx';
-import Auth from './pages/Auth.jsx';
-import Home from './pages/Home.jsx'; // Import the Home component
+import App from './App';
+import Home from './pages/Home';
+import Auth from './pages/Auth';
+import BookClub from './pages/BookClub';
+import Profile from './pages/Profile';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    children: [
-      {
-        path: '/home',
-        element: <Home />
-      },
-      {
-        path: '/auth',
-        element: <Auth />
-      },
-    ],
-  },
-]);
-
-// Change ReactDOM.createRoot to ReactDOM.createRoot(document.getElementById('root'))
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router} />
+ReactDOM.render(
+  <Router>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route path="/home" element={<Home />} />
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/bookclub/:bookClubId" element={<BookClub />} />
+        <Route path="profile/:username" element={<Profile />} />
+      </Route>
+    </Routes>
+  </Router>,
+  document.getElementById('root')
 );
