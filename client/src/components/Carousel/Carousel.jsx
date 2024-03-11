3030f071a407de73713a9f658bf56a00627c376e
Jessmica11 <116199836+Jessmica11@users.noreply.github.com>
2024-03-11 12:26:54 -0400

    Slides now link to their pages

--- client/src/components/Carousel/Carousel.jsx
+++ client/src/components/Carousel/Carousel.jsx
@@ -1,55 +1,18 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../Carousel/Carousel.css';
-
-const _items = [
-  {
-    bookclub: {
-      name: 'Bros & Books',
-      desc: 'In this book, blogger and former internet entrepreneur Mark Manson explains in simple, no expletives barred terms how to achieve happiness...',
-      image: 'https://ia902501.us.archive.org/BookReader/BookReaderPreview.php?id=subtleartofnotgi0000mans_n2n5&itemPath=%2F15%2Fitems%2Fsubtleartofnotgi0000mans_n2n5&server=ia902501.us.archive.org&page=cover_w500_h500.jpg',
-    },
-  },
-  {
-    bookclub: {
-      name: "A Novel Idea",
-      desc: "For anyone who needs reminding that a life worth living can only be born from radical honesty...",
-      image: 'https://ia804703.us.archive.org/view_archive.php?archive=/9/items/l_covers_0012/l_covers_0012_88.zip&file=0012881449-L.jpg',
-    },
-  },
-  {
-    bookclub: {
-      name: 'Between the Lines',
-      desc: 'Alicia Berenson’s life is seemingly perfect. One evening her husband Gabriel returns home late from a fashion shoot, and Alicia shoots him five times in the face, and then never speaks another word...',
-      image: 'https://ia801401.us.archive.org/view_archive.php?archive=/32/items/l_covers_0008/l_covers_0008_41.tar&file=0008415060-L.jpg',
-    },
-  },
-  {
-    bookclub: {
-      name: 'Smutty Buddies',
-      desc: 'Mike Sigel or "The unworldly, innocent Ana is startled to realize she wants this man and, despite his enigmatic reserve, finds she is desperate to get close to him...',
-      image: 'https://covers.openlibrary.org/b/id/14566671-L.jpg',
-    },
-  },
-  {
-    bookclub: {
-      name: 'Bookworms',
-      desc: 'When two young rival journalists find love through a magical connection, they must face the depths of hell, in a war among gods, to seal their fate forever...',
-      image: 'https://ia801909.us.archive.org/view_archive.php?archive=/31/items/l_covers_0013/l_covers_0013_26.zip&file=0013268161-L.jpg',
-    },
-  },
-];
+import bookClubsData from '../BookClubs/BookClubsData';
 
 const CarouselSlideItem = ({ item, active }) => {
   return (
     <li className={`carousel__slide-item ${active ? 'active' : ''}`}>
-      <Link to={`/book-club/${item.bookclub.id}`}>
+      <Link to={`/book-club/${item.id}`}> 
         <div className="carousel__slide-item-img-link">
-          <img src={item.bookclub.image} alt={item.bookclub.name} />
+          <img src={item.image} alt={item.name} />
         </div>
         <div className="carousel-slide-item__body">
-          <h4>{item.bookclub.name}</h4>
-          <p>{item.bookclub.desc}</p>
+          <h4>{item.name}</h4>
+          <p>{item.desc}</p>
         </div>
       </Link>
     </li>
@@ -64,7 +27,7 @@ const Carousel = () => {
       <div className="carousel__inner">
         <div className="carousel__container">
           <ul className="carousel__slide-list">
-            {_items.map((item, i) => (
+            {bookClubsData.map((item, i) => (
               <CarouselSlideItem
                 key={i}
                 item={item}
