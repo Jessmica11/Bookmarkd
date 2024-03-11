2e682805c5a5417fe3fccf6d91fea0cd9e1cd280
Jessmica11 <116199836+Jessmica11@users.noreply.github.com>
2024-03-11 15:48:33 -0400

    Connected this component to BookClubsData.js

--- client/src/components/BookClubs/BookClubDetails.jsx
+++ client/src/components/BookClubs/BookClubDetails.jsx
@@ -1,27 +1,20 @@
-import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
 import React from 'react';
-import { GET_BOOK_CLUB_DETAILS } from './queries';
+import bookClubsData from '../BookClubs/BookClubsData';
 
-const BookClubDetails = () => {
-  // uses `useParams` to retrieve value of the route parameter `:bookClubId`
-  const { bookClubId } = useParams();
-  // this will return an object with three properties: `loading`, `error`, and `data`
-  const { loading, error, data } = useQuery(GET_BOOK_CLUB_DETAILS, {
-    variables: { bookClubId },
-  });
+const BookClubDetails = ({ bookClubId }) => {
+  // Find the book club details based on the bookClubId
+  const bookClub = bookClubsData.find(club => club.id === bookClubId);
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error: {error.message}</p>;
-
-  // if successful, the `data` object will contain the `bookClubById` data
-  const bookClub = data.bookClubById; 
+  // if not found for some reason, return a message to the user on the screen
+  if (!bookClub) {
+    return <div>Book club not found!</div>;
+  }
 
   return (
-    // this will return the book club details
     <div>
-      <h1>Book Club {bookClubId}</h1>
+      <h1>{bookClub.name}</h1>
+      <img src={bookClub.image} alt={bookClub.name} />
+      <p>{bookClub.desc}</p>
     </div>
   );
 };
